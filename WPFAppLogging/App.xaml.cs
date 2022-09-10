bb38534764f0ae4705333c5c92cce94bee1c6f2b
wcrowe <crowe.will@gmail.com>
2022-09-09 20:40:20 -0400

    initial

---
+++ WPFAppLogging/App.xaml.cs
@@ -0,0 +1,6 @@
+﻿namespace WPFAppLogging {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App { }
+}
---
+++ WPFAppLogging/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+using NLog;
+using NLog.Fluent;
+
+namespace WPFAppLogging {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow {
+        public static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+   
+        public MainWindow() {
+            InitializeComponent();
+            DoLog();
+        }
+
+        private void DoLog() {
+
+
+        
+            Logger.Info("App Starting");
+         
+        }
+    }
+}
